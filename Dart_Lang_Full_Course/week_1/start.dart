ca9f2237737df94a7c0f65541516f375a8bb41f1
Smtrbci <75490736+Smtrbci@users.noreply.github.com>
2022-04-03 16:07:47 +0300

    week 1

---
+++ Dart_Lang_Full_Course/week_1/start.dart
@@ -0,0 +1,21 @@
+void main(){
+  print("Hello my first project");
+
+  print("Hello" + "Samet");
+  print(2 + 3);
+
+  String verininAdi = "verinin Degeri";
+
+  String hello = "hello3";
+
+  print(hello);
+  print(hello);
+  print(hello);
+  print(hello);
+  print(hello);
+
+  int money = 25;
+  print (money);
+
+  print(hello + "money");
+}
