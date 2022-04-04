d4c8728317d4f0431f0c6c12922de9e69cbdbe02
Smtrbci <75490736+Smtrbci@users.noreply.github.com>
2022-04-05 00:16:00 +0300

    week1

---
+++ Dart_Lang_Full_Course/week_1/user_advance.dart
@@ -0,0 +1,11 @@
+void main() {
+  final String userName = "samet";
+
+  final int bankMonay = 100;
+  const String bankName = "SA Bank";
+
+  var userName2 = "samet2";
+  var userName2Monay = 15;
+
+  userName2Monay -= 10;
+}
--- Dart_Lang_Full_Course/week_1/user_basic.dart
+++ Dart_Lang_Full_Course/week_1/user_basic.dart
@@ -1,3 +1,24 @@
 void main() {
+  print("Merhaba" + '${25 + 5}');
 
+  int userMonay = 25;
+  String userName = "Samet";
+  userMonay = userMonay + 5;
+
+  print("Merhaba $userName $userMonay");
+
+  print('------');
+  userMonay = userMonay - 10;
+  print('Paraniz deger kaybetti $userMonay');
+
+  userMonay = userMonay + 5;
+  userMonay += 5;
+
+  userMonay = userMonay  ~/ 2;
+
+  double sametMonay = 15.2;
+
+  sametMonay = sametMonay / 2;
+  print('Samet bey paraniz uctu $sametMonay');
+  print('Veli bey paraniz uctu $userMonay');
 }
